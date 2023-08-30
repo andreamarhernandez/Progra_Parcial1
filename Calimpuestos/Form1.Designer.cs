d57d7ec4afe314e2583a24ab2017f9543029640d
Andrea Margarita Hernández Arévalo <USSS023623@ugb.edu.sv>
2023-08-29 18:55:01 -0600

    Agregar archivos de proyecto.

---
+++ Calimpuestos/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Calimpuestos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IMPUESTO A LAS ACTIVIDADES ECONOMICAS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Monto:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Valor a Pagar";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(180, 93);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(100, 20);
+            this.TextBox1.TabIndex = 3;
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(180, 132);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(100, 20);
+            this.TextBox2.TabIndex = 4;
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(161, 207);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(75, 23);
+            this.Button1.TabIndex = 5;
+            this.Button1.Text = "Calcular";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 296);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.TextBox TextBox2;
+        private System.Windows.Forms.Button Button1;
+    }
+}
+
---
+++ Calimpuestos/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calimpuestos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // Obtener el monto de la actividad económica del cuadro de texto.
+            double monto = double.Parse(TextBox1.Text);
+
+            // Calcular el impuesto.
+            double impuesto = 0;
+
+            // Recorrer la tabla de precios.
+            for (int i = 0; i < Precios.Length; i++)
+            {
+                // Si el monto está dentro del rango de precios actual.
+                if (monto >= Rangos[i] && monto < Rangos[i + 1])
+                {
+                    // Calcular el impuesto.
+                    impuesto = Precios[i] + CostosAdicional[i] * (monto - Rangos[i]) / 1000;
+                    break;
+                }
+            }
+
+            // Mostrar el impuesto en el cuadro de texto.
+            TextBox2.Text = impuesto.ToString("C2");
+        }
+
+        private static double[] Precios = new double[] { 1.5, 1.5, 3, 6, 9, 15, 39, 63, 93, 125, 195, 255 };
+        private static double[] Rangos = new double[] { 0.01, 500.01, 1000.01, 2000.01, 3000.01, 8000.01, 18000, 01, 30000.01, 60000.01, 100000.01, 200000.01, 300000.01 };
+        private static double[] CostosAdicional = new double[] { 0, 3, 3, 3, 2, 2, 2, 1, 0.8, 0.7, 0.6, 0.45 };
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+        
